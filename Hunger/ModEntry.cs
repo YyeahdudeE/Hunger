37009070d85c92a9289fd513a157ef2b1f906b2a
YyeahdudeE <davevkelly91@yahoo.com>
2019-12-14 15:18:15 -0500

    - Updated for Stardew 1.4 - Combined mail content pack and smapi mod into one single mod with a new option in the config file to enable/disable the optional mail Hunger sends you.

--- Hunger/ModEntry.cs
+++ Hunger/ModEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
@@ -8,101 +9,122 @@ namespace Hunger
 {
     public class ModEntry : Mod
     {
+        private float ticksPerHour = 0f;
+        private float gainAmount = 0f;
+        private float drainAmount = 0f;
+        private ModConfig config;
+
         public override void Entry(IModHelper helper)
         {
-            ModConfig config = helper.ReadConfig<ModConfig>();
-            GameEvents.EighthUpdateTick += this.EighthUpdateTick;
+            this.config = helper.ReadConfig<ModConfig>();
+            ticksPerHour = 8 * config.RealSecondsPerInGame10Minutes * 6;
+            Helper.Events.GameLoop.UpdateTicked += GameLoop_UpdateTicked;
+            Helper.Events.GameLoop.GameLaunched += GameLoop_GameLaunched;
         }
 
-        private void EighthUpdateTick(object sender, EventArgs e)
+        private void GameLoop_GameLaunched(object sender, GameLaunchedEventArgs e)
         {
-            ModConfig config = this.Helper.ReadConfig<ModConfig>();
-
-            if (Game1.player == null || Game1.hasLoadedGame == false || Game1.isFestival()  || Game1.activeClickableMenu !=  null || Game1.currentMinigame != null || Game1.dialogueUp == true || Game1.eventUp == true || !Game1.game1.IsActive)
-                return;
-
-            float ticksPerHour = 8 * config.realSecondsPerInGame10Minutes * 6;
-
-            if (Game1.currentLocation.name == "FarmHouse")
+            if (config.HungerMailEnabled)
             {
-                float gainAmount = 0f;
-                switch (Game1.player.houseUpgradeLevel)
+                Helper.Content.AssetEditors.Add(new ModMail());
+            }
+        }
+
+        private void GameLoop_UpdateTicked(object sender, UpdateTickedEventArgs e)
+        {
+
+            if (e.IsMultipleOf(8))
+            {
+
+                if (Game1.player == null || Game1.hasLoadedGame == false || Game1.isFestival() || Game1.activeClickableMenu != null || Game1.currentMinigame != null || Game1.dialogueUp == true || Game1.eventUp == true || !Game1.game1.IsActive)
+                    return;
+
+                if (Game1.currentLocation.Name == "FarmHouse")
                 {
-                    case 0:
-                        gainAmount = config.insidePlayerHomeRegenerationPerHourFarmLevel0 / ticksPerHour;
-                        break;
-                    case 1:
-                        gainAmount = config.insidePlayerHomeRegenerationPerHourFarmLevel1 / ticksPerHour;
-                        break;
-                    case 2:
-                        gainAmount = config.insidePlayerHomeRegenerationPerHourFarmLevel2 / ticksPerHour;
-                        break;
-                    case 3:
-                        gainAmount = config.insidePlayerHomeRegenerationPerHourFarmLevel3 / ticksPerHour;
-                        break;
-                }
-                if (gainAmount + Game1.player.stamina >= Game1.player.maxStamina)
-                {
-                    Game1.player.stamina = Game1.player.maxStamina;
+                    gainAmount = 0f;
+                    switch (Game1.player.HouseUpgradeLevel)
+                    {
+                        case 0:
+                            gainAmount = config.InsidePlayerHomeRegenerationPerHourFarmLevel0 / ticksPerHour;
+                            break;
+                        case 1:
+                            gainAmount = config.InsidePlayerHomeRegenerationPerHourFarmLevel1 / ticksPerHour;
+                            break;
+                        case 2:
+                            gainAmount = config.InsidePlayerHomeRegenerationPerHourFarmLevel2 / ticksPerHour;
+                            break;
+                        case 3:
+                            gainAmount = config.InsidePlayerHomeRegenerationPerHourFarmLevel3 / ticksPerHour;
+                            break;
+                        default:
+                            break;
+                    }
+                    if (gainAmount + Game1.player.stamina >= Game1.player.maxStamina.Value)
+                    {
+                        Game1.player.stamina = Game1.player.maxStamina.Value;
+                    }
+                    else
+                    {
+                        Game1.player.stamina += gainAmount;
+                    }
                 }
                 else
                 {
-                    Game1.player.stamina += gainAmount;
-                }
-            }
-            else
-            {
-                float drainAmount = 0f;
-                switch (Game1.player.houseUpgradeLevel)
-                {
-                    case 0:
-                        drainAmount = config.standingStillStaminaDrainPerHourFarmLevel0 / ticksPerHour;
-                        break;
-                    case 1:
-                        drainAmount = config.standingStillStaminaDrainPerHourFarmLevel1/ ticksPerHour;
-                        break;
-                    case 2:
-                        drainAmount = config.standingStillStaminaDrainPerHourFarmLevel2 / ticksPerHour;
-                        break;
-                    case 3:
-                        drainAmount = config.standingStillStaminaDrainPerHourFarmLevel3 / ticksPerHour;
-                        break;
-                }
+                    drainAmount = 0f;
+                    switch (Game1.player.HouseUpgradeLevel)
+                    {
+                        case 0:
+                            drainAmount = config.StandingStillStaminaDrainPerHourFarmLevel0 / ticksPerHour;
+                            break;
+                        case 1:
+                            drainAmount = config.StandingStillStaminaDrainPerHourFarmLevel1 / ticksPerHour;
+                            break;
+                        case 2:
+                            drainAmount = config.StandingStillStaminaDrainPerHourFarmLevel2 / ticksPerHour;
+                            break;
+                        case 3:
+                            drainAmount = config.StandingStillStaminaDrainPerHourFarmLevel3 / ticksPerHour;
+                            break;
+                    }
 
-                if (Game1.player.isMoving() && !Game1.player.isRidingHorse())
-                {
-                    drainAmount *= config.walkingMultiplier;
-                }
-                if (Game1.currentLocation.name == "UndergroundMine")
-                {
-                    drainAmount *= config.inMineMultiplier;
-                }
-                if (Game1.timeOfDay >= 2400)
-                {
-                    drainAmount *= config.afterMidnightMultiplier;
-                }
+                    if (Game1.player.isMoving() && !Game1.player.isRidingHorse())
+                    {
+                        drainAmount *= config.WalkingMultiplier;
+                    }
+                    if (Game1.currentLocation.Name == "UndergroundMine")
+                    {
+                        drainAmount *= config.InMineMultiplier;
+                    }
+                    if (Game1.timeOfDay >= 2400)
+                    {
+                        drainAmount *= config.AfterMidnightMultiplier;
+                    }
 
-                Game1.player.stamina -= drainAmount;
+                    Game1.player.stamina -= drainAmount;
+                }
             }
         }
+
     }
 
     class ModConfig
     {
-        public float insidePlayerHomeRegenerationPerHourFarmLevel0 { get; set; } = 110;
-        public float insidePlayerHomeRegenerationPerHourFarmLevel1 { get; set; } = 135;
-        public float insidePlayerHomeRegenerationPerHourFarmLevel2 { get; set; } = 160;
-        public float insidePlayerHomeRegenerationPerHourFarmLevel3 { get; set; } = 185;
+        public float InsidePlayerHomeRegenerationPerHourFarmLevel0 { get; set; } = 110;
+        public float InsidePlayerHomeRegenerationPerHourFarmLevel1 { get; set; } = 135;
+        public float InsidePlayerHomeRegenerationPerHourFarmLevel2 { get; set; } = 160;
+        public float InsidePlayerHomeRegenerationPerHourFarmLevel3 { get; set; } = 185;
 
-        public float standingStillStaminaDrainPerHourFarmLevel0 { get; set; } = 3;
-        public float standingStillStaminaDrainPerHourFarmLevel1 { get; set; } = 9;
-        public float standingStillStaminaDrainPerHourFarmLevel2 { get; set; } = 12;
-        public float standingStillStaminaDrainPerHourFarmLevel3 { get; set; } = 15;
+        public float StandingStillStaminaDrainPerHourFarmLevel0 { get; set; } = 3;
+        public float StandingStillStaminaDrainPerHourFarmLevel1 { get; set; } = 9;
+        public float StandingStillStaminaDrainPerHourFarmLevel2 { get; set; } = 12;
+        public float StandingStillStaminaDrainPerHourFarmLevel3 { get; set; } = 15;
 
-        public float walkingMultiplier { get; set; } = 2;
-        public float inMineMultiplier { get; set; } = 2;
-        public float afterMidnightMultiplier { get; set; } = 1.5f;
+        public float WalkingMultiplier { get; set; } = 2;
+        public float InMineMultiplier { get; set; } = 2;
+        public float AfterMidnightMultiplier { get; set; } = 1.5f;
 
-        public float realSecondsPerInGame10Minutes { get; set; } = 7;
+        public float RealSecondsPerInGame10Minutes { get; set; } = 7;
+
+        public bool HungerMailEnabled { get; set; } = true;
     }
 }
---
+++ Hunger/ModMail.cs
@@ -0,0 +1,31 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunger
+{
+    class ModMail : IAssetEditor
+    {
+        public ModMail() { }
+
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+            return asset.AssetNameEquals("Data\\mail");
+        }
+
+        public void Edit<T>(IAssetData asset)
+        {
+            var data = asset.AsDictionary<string, string>().Data;
+
+            data["spring_3_1"] = "Hey there @,^Starting to feel HUNGRY yet? Maybe it's your REAL LIFE HUMAN boy/girl STOMACH reminding you that you gotta EAT to live.^These should help keep it quiet for a few days.^^   -Hunger %item object 194 5 %%";
+            data["spring_9_1"] = "Hello agin,^The spirits seem to be warning me about RAIN in the near future, either that or I watch way too much TV...^EAT these to get the most out of your next rainy day, or SELL THEM, I DON'T CARE!^^   -Hunger %item object 731 2 %%";
+            data["spring_15_1"] = "@,^Keep an eye out the next couple days for ANTIOXIDANT RICH BERRIES on bushes around town to fuel your busy life.^Here's a few to get you hooked...That was in poor TASTE, sorry!^^   -Hunger %item object 296 7 %%";
+            data["spring_28_1"] = "Hey @,^How was your first month of being a REAL LIFE farmer? Just kidding, I know you can't answer me!^Hope this comes in handy for your next big seed plating session.You know BREAKFAST IS THE MOST IMPOR...Well see ya around.^^   -Hunger^P.Sssst. Check out that small bush behind the playground at exactly noon today!  %item object 201 1 %%";
+            data["spring_1_2"] = "@,^LONG TIME NO SEE!...Who are we kidding, I've been watching you this whole time.^FRUIT SALAD?^^   -That Dragon, Hunger %item object 610 4 %%";
+            data["fall_2_1"] = "@,^^One for you, one for a FRIEND.^^   -Hunger %item object 218 2 732 2 220 2 226 2 608 2 205 2 224 2 %%";
+        }
+    }
+}
